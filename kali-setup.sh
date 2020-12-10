ae13db986c8cb71f7266f8fafea088474c819765
James Smith <jamessmith1@outlook.com>
2020-12-10 22:09:11 +0000

    Update to V2

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 clear
 #Vars
-gitdir=/root/Tools
-dir1=/root/Tools/hcxtools
-dir2=/root/Tools/hcxdumptool
+gitdir=/opt/tools
+mobiledir=/opt/tools/mobile
+clouddir=/opt/tools/cloud
+wirelessdir=/opt/tools/wireless
+reportingdir=/opt/tools/reportingdir
+wordlistsdir=/opt/tools/wordlists
+containersdir=/opt/tools/containers
+osintdir=/opt/tools/osint
+enumdir=/opt/tools/enumeration
+cheatsheetsdir=/opt/tools/cheetsheets
+dir1=/opt/tools/wireless/hcxtools
+dir2=/opt/tools/wireless/hcxdumptool
 mkdir -p $gitdir
+mkdir -p $clouddir
+mkdir -p $mobiledir
+mkdir -p $wirelessdir
+mkdir -p $reportingdir
+mkdir -p $wordlistsdir
+mkdir -p $containersdir
+mkdir -p $osintdir
+mkdir -p $enumdir
+mkdir -p $cheatsheets
 
 #Handling
 die() { echo "$@" 1>&2; exit 1; }
@@ -18,7 +36,7 @@ fi
 PS3='Please enter your choice: '
 
 #input options
-options=("1-Post-Install-Upgrade" "2-Dist-Upgrade" "3-Install-Packages" "4-Install-Git-Repos" "5-Install-Oh-My-ZSH" "6-Install-Vbox-Additions" "7-Install-Wifite-Dependencies" "8-Update-Git-Tools" "9-Exit")
+options=("1-Post-Install-Upgrade" "2-Dist-Upgrade" "3-Install-Packages" "4-Install-Git-Repos" 5-Install-Melcara-Nessus-Parser" "6-Install-Wifite-Dependencies" "7-Update-Git-Tools" "8-Exit")
 
 #function to display options nice
 function optionsClean() {
@@ -27,8 +45,8 @@ function optionsClean() {
 	done
 }
 #Menu Headers
-printf "\033[1;41m  Kali Post Install Script - Version 0.1\033[1;m\n\033[1;46m  By James Smith | https://www.stealingthe.network | @iamstx  \033[1;m\n"
-printf "\033[1;46m  If you've just installed Kali Version 2, It's recommended to go with Option 1 first.  \033[1;m\n"
+printf "\033[1;41m  Kali Post Install Script - Version 2.0\033[1;m\n\033[1;46m  By James Smith | https://www.stealingthe.network | @osiris2600  \033[1;m\n"
+printf "\033[1;46m  If you've just installed Kali Version 2020, It's recommended to go with Option 1 first.  \033[1;m\n"
 
 #define what to do for each option
 select opt in "${options[@]}"
@@ -61,7 +79,7 @@ do
 		;;
 		"3-Install-Packages")
 		printf "\033[1;41m Starting apt-get update...\x1b[0m\n\n"
-		apt install tor libreoffice brutespray crackmapexec bloodhound ftp secure-delete flameshot bleachbit veil veil-evasion nfs-common onesixtyone finger rsh-client jxplorer sipcalc nbtscan recordmydesktop eyewitness filezilla tmux zsh testssl.sh freerdp2-x11 remmina neo4j bettercap seclists shellter backdoor-factory responder firmware-mod-kit gobuster
+		apt install tor libreoffice brutespray crackmapexec bloodhound ftp secure-delete flameshot bleachbit veil veil-evasion nfs-common onesixtyone finger rsh-client jxplorer sipcalc nbtscan recordmydesktop eyewitness filezilla tmux zsh testssl.sh freerdp2-x11 remmina neo4j bettercap seclists shellter backdoor-factory responder firmware-mod-kit gobuster docker.io
 		printf "\033[1;41m Starting apt-get install -y linux-headers-$(uname -r)...\x1b[0m\n\n"
 		apt-get install -y linux-headers-$(uname -r)
 		printf "\033[1;41m Done!\x1b[0m\n\n"
@@ -71,72 +89,44 @@ do
 		printf "\033[1;41m Checking for Tools Directory...(Repo's will be cloned to /root/Tools) \x1b[0m\n\n"
 		printf "\033[1;41m Cloning Git Repo's...\x1b[0m\n\n"
 		cd $gitdir
-		git clone https://github.com/0xInfection/TIDoS-Framework
-		git clone https://github.com/RhinoSecurityLabs/pacu
-		git clone https://github.com/m0rtem/CloudFail
-		git clone https://github.com/trustedsec/ridenum
+		git clone https://github.com/m0rtem/CloudFail $clouddir
 		git clone https://github.com/pentestmonkey/windows-privesc-check
-		git clone https://github.com/trustedsec/unicorn
-		git clone https://github.com/ICSec/airpwn-ng
-		git clone https://github.com/adaptivethreat/Empire
-		git clone https://github.com/DanMcInerney/wifijammer
-		git clone https://github.com/CoreSecurity/impacket
-		git clone https://github.com/leebaird/discover
-		git clone https://github.com/scrapy/scrapy
-		git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF
-		git clone https://github.com/drwetter/testssl.sh/
+		git clone https://github.com/ICSec/airpwn-ng $wirelessdir
+		git clone https://github.com/DanMcInerney/wifijammer $wirelessdir
+		git clone https://github.com/leebaird/discover $osintdir
+		git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF $mobiledir
 		git clone https://github.com/BishopFox/spoofcheck
-		git clone https://github.com/DanMcInerney/icebreaker
-		git clone https://github.com/hak5darren/wp6
-		git clone https://github.com/superkojiman/onetwopunch
-		git clone https://github.com/nccgroup/chuckle
-		git clone https://github.com/m3m1t1m/appalyzer
-		git clone https://github.com/adipinto/yet-another-nessus-parser
-		git clone https://github.com/thelinuxchoice/blackeye
-		git clone https://github.com/securestate/king-phisher
-		git clone https://github.com/shenril/Sitadel
-		git clone https://github.com/khast3x/h8mail
-		git clone https://github.com/aquasecurity/kube-hunter
-		git clone https://github.com/BlackArch/wordlistctl
-		git clone https://github.com/ThoughtfulDev/EagleEye
-		git clone https://github.com/cr4shcod3/pureblood
-		git clone https://github.com/vaguileradiaz/tinfoleak
-		git clone https://github.com/davidtavarez/pwndb
-		git clone https://github.com/21y4d/nmapAutomator
-		git clone https://github.com/ivan-sincek/invoker
-		git clone https://github.com/dirkjanm/ROADtools
-		git clone https://github.com/swisskyrepo/PayloadsAllTheThings
-		wget https://sec.stanev.org/download/wlc/wlc_0.2.zip
+		git clone https://github.com/hak5darren/wp6 $wirelessdir
+		git clone https://github.com/aquasecurity/kube-hunter $containersdir
+		git clone https://github.com/davidtavarez/pwndb $osintdir
+		git clone  https://github.com/phx/pyusbmux.git $mobiledir
+		printf "\033[1;41m Installing Objection and Frida via Pip!\x1b[0m\n\n"
+		pip3 install objection
+		pip3 install frida
+		printf "\033[1;41m Pulling MobSF Docker Image!\x1b[0m\n\n"
+		docker pull opensecurity/mobile-security-framework-mobsf
 		printf "\033[1;41m Done!\x1b[0m\n\n"
 		optionsClean
 		;;
-		"5-Install-Oh-My-ZSH")
-		printf "\033[1;41m Installing Oh My ZSH!...\x1b[0m\n\n"
-		wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P /tmp
-		sed -i 's/RUNZSH=${RUNZSH:-yes}/RUNZSH=${RUNZSH:-no}/g' /tmp/install.sh
-		chmod +x /tmp/install.sh
-		/bin/bash /tmp/install.sh
-		printf "\033[1;41m Done!!...\x1b[0m\n\n"
-		printf "\033[1;41m Changing the ZSH theme..\x1b[0m\n\n"
-		sed -i 's/robbyrussell/jonathan/g' /root/.zshrc
-		printf "\033[1;41m Completed.. you will need to restart your terminal to launch ZSH\x1b[0m\n\n"
-		rm /tmp/install.sh
+		"5-Install-Melcara-Nessus-Parser")
+		printf "\033[1;41m Installing Melcara Nessus Parser...\x1b[0m\n\n"
+		export PERL_MM_USE_DEFAULT=1
+		mkdir /opt/melcara && cd /opt/melcara
+		wget http://www.melcara.com/wp-content/uploads/2017/09/parse_nessus_xml.v24.pl_.zip
+		unzip parse_nessus_xml.v24.pl_.zip
+		chmod +x parse_nessus_xml.v24.pl
+		cpan install Excel::Writer::XLSX XML::Simple XML::TreePP Math::Round Excel::Writer Data::Table
+		rm parse_nessus_xml.v24.pl_.zip
+		printf "\033[1;41m Done! .\x1b[0m\n\n"
 		optionsClean
 		;;
-		"6-Install-Vbox-Additions")
-		printf "\033[1;41m Installing VirtualBOx Guest Additions...\x1b[0m\n\n"
-		apt-get update
-		apt-get install virtualbox-guest-x11 -y
-		printf "\033[1;41m Done! You will need to reboot for the changes to take effect.\x1b[0m\n\n"
-		optionsClean
-		;;
-		"7-Install-Wifite-Dependencies")
+		"6-Install-Wifite-Dependencies")
 		printf "\033[1;41m Checking some packages...\x1b[0m\n\n"
 		apt install libcurl4-openssl-dev libssl-dev zlib1g-dev
 		printf "\033[1;41m Cloning Wifite Dependencies from GitHub...\x1b[0m\n\n"
 		cd $gitdir
-		git clone https://github.com/ZerBea/hcxtools
-		git clone https://github.com/ZerBea/hcxdumptool
+		git clone https://github.com/ZerBea/hcxtools $wirelessdir
+		git clone https://github.com/ZerBea/hcxdumptool $wirelessdir
 		cd $dir2
 		make
 		make install
@@ -147,15 +137,15 @@ do
 		printf "\033[1;41m hcxtools installed!...\x1b[0m\n\n"
 		optionsClean
 		;;
-		"8-Update-Git-Tools")
-                printf "\033[1;41m Performing a Git Pull on all Repo's...\x1b[0m\n\n"
+		"7-Update-Git-Tools")
+        printf "\033[1;41m Performing a Git Pull on all Repo's...\x1b[0m\n\n"
 		printf "\033[1;41m You may see multiple lines of 'already up to date' this is normal.\x1b[0m\n\n"
-                cd $gitdir
-                ls | xargs -I{} git -C {} pull
+        cd $gitdir
+        ls | xargs -I{} git -C {} pull
 		printf "\033[1;41m All Done...\x1b[0m\n\n"
-                optionsClean
+        optionsClean
 		;;
-		"9-Exit")
+		"8-Exit")
 		break
 		;;
 		*) Invalid option ;;
